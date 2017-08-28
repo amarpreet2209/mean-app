b6421c5544f269570fdc168f52457971176510f8
Amarpreet Singh <amarpreet2209@gmail.com>
2017-08-28 14:40:27 +0530

    53

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,3 +1,6 @@
+import { routing } from './app.routing';
+import { HeaderComponent } from './header.component';
+import { MessagesComponent } from './messages/messages.component';
 import { MessageListComponent } from './messages/message-list.component';
 import { MessageComponent } from './messages/message.component';
 import { NgModule } from '@angular/core';
@@ -6,15 +9,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
 import { MessageInputComponent } from './messages/message-input.component';
+import { AuthenticationComponent } from './auth/authentication.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         MessageComponent,
         MessageListComponent,
-        MessageInputComponent
+        MessageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent,
+        HeaderComponent
     ],
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, routing],
     bootstrap: [AppComponent]
 })
 export class AppModule {
